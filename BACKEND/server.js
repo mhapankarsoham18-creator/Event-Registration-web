7c4b85235f38ff750da1b3886d417757343506dc
Soham Mhapankar <mhapankar.soham18@gamil.com>
2026-02-24 15:39:26 +0530

    final cors

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://127.0.0.1:5500",
-    "https://YOUR-NETLIFY-LINK.netlify.app"
+    "https://singular-biscochitos-835e88.netlify.app"
   ],
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
